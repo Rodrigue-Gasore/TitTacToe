bf0a68553d7deefd948d76d37f3e755d387307cd
Rodrigue <r.gasore@bk.rw>
2020-01-10 21:05:19 +0200

    updates

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,29 +1,22 @@
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.util.Random;
 
+public class TicTacToe {
 
-public class TicTacToe extends JFrame {
     //game board
     public static final int ROWS = 3;
     public static final int COLS = 3;
-
-    public static final int CELL_SIZE = 100;
-    public static final int CANVAS_WIDTH = CELL_SIZE * COLS;
-    public static final int CANVAS_HEIGHT = CELL_SIZE * ROWS;
-    public static final int GRID_WIDTH = 8;
-    public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2;
-
-    public static final int CELL_PADDING = CELL_SIZE / 6;
-    public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING * 2;
-    public static final int SYMBOL_STROKE_WIDTH = 8;
-
 
     public enum GameState {
         PLAYING, DRAW, CROSS_WON, NOUGHT_WON
     }
     private GameState currentState;
 
+    public enum GameOption{
+        TWO_PlAYERS, ONE_PLAYER
+    }
+
+    private GameOption currentOption;
 
     public enum Seed {
         EMPTY, CROSS, NOUGHT
@@ -31,58 +24,13 @@ public class TicTacToe extends JFrame {
     private Seed currentPlayer;
 
     private Seed[][] board   ;
-    private DrawCanvas canvas;
-    private JLabel statusBar;
 
-
-    public TicTacToe() {
-        canvas = new DrawCanvas();
-        canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
-
-        canvas.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int mouseX = e.getX();
-                int mouseY = e.getY();
-
-                int rowSelected = mouseY / CELL_SIZE;
-                int colSelected = mouseX / CELL_SIZE;
-
-                if (currentState == GameState.PLAYING) {
-                    if (rowSelected >= 0 && rowSelected < ROWS && colSelected >= 0
-                            && colSelected < COLS && board[rowSelected][colSelected] == Seed.EMPTY) {
-                        board[rowSelected][colSelected] = currentPlayer;
-                        updateGame(currentPlayer, rowSelected, colSelected);
-
-                        currentPlayer = (currentPlayer == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
-                    }
-                } else {
-                    initGame();
-                }
-
-                repaint();
-            }
-        });
-
-
-        statusBar = new JLabel("  ");
-        statusBar.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 15));
-        statusBar.setBorder(BorderFactory.createEmptyBorder(2, 5, 4, 5));
-
-        Container cp = getContentPane();
-        cp.setLayout(new BorderLayout());
-        cp.add(canvas, BorderLayout.CENTER);
-        cp.add(statusBar, BorderLayout.PAGE_END);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setTitle("Tic Tac Toe");
-        setVisible(true);
-
-        board = new Seed[ROWS][COLS];
-        initGame();
+    public TicTacToe(GameState currentState, GameOption currentOption, Seed currentPlayer, Seed[][] board){
+        this.currentState=currentState;
+        this.currentOption=currentOption;
+        this.currentPlayer=currentPlayer;
+        this.board=board;
     }
-
 
     public void initGame() {
         for (int row = 0; row < ROWS; ++row) {
@@ -102,6 +50,29 @@ public class TicTacToe extends JFrame {
             currentState = GameState.DRAW;
         }
 
+    }
+
+    public Point computerMove(Seed[][] board){
+        Point position = null;
+
+        Random randomRow;
+        Random randomCol;
+        Boolean freeSeed = false;
+        int row=0;
+        int col=0;
+        while (!freeSeed){
+            randomRow = new Random();
+            row=randomRow.nextInt(this.ROWS);
+            System.out.println("row "+row);
+            randomCol = new Random();
+            col=randomCol.nextInt(this.COLS);
+            System.out.println("col "+col);
+            if(board[row][col]== Seed.EMPTY){
+                position = new Point(row,col);
+                freeSeed=true;
+            }
+        }
+        return position;
     }
 
 
@@ -134,71 +105,35 @@ public class TicTacToe extends JFrame {
                 && board[2][0] == theSeed);
     }
 
-
-    class DrawCanvas extends JPanel {
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            setBackground(Color.WHITE);
-
-            g.setColor(Color.LIGHT_GRAY);
-            for (int row = 1; row < ROWS; ++row) {
-                g.fillRoundRect(0, CELL_SIZE * row - GRID_WIDHT_HALF,
-                        CANVAS_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
-            }
-            for (int col = 1; col < COLS; ++col) {
-                g.fillRoundRect(CELL_SIZE * col - GRID_WIDHT_HALF, 0,
-                        GRID_WIDTH, CANVAS_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
-            }
-
-            Graphics2D g2d = (Graphics2D)g;
-            g2d.setStroke(new BasicStroke(SYMBOL_STROKE_WIDTH, BasicStroke.CAP_ROUND,
-                    BasicStroke.JOIN_ROUND));  // Graphics2D only
-            for (int row = 0; row < ROWS; ++row) {
-                for (int col = 0; col < COLS; ++col) {
-                    int x1 = col * CELL_SIZE + CELL_PADDING;
-                    int y1 = row * CELL_SIZE + CELL_PADDING;
-                    if (board[row][col] == Seed.CROSS) {
-                        g2d.setColor(Color.RED);
-                        int x2 = (col + 1) * CELL_SIZE - CELL_PADDING;
-                        int y2 = (row + 1) * CELL_SIZE - CELL_PADDING;
-                        g2d.drawLine(x1, y1, x2, y2);
-                        g2d.drawLine(x2, y1, x1, y2);
-                    } else if (board[row][col] == Seed.NOUGHT) {
-                        g2d.setColor(Color.BLUE);
-                        g2d.drawOval(x1, y1, SYMBOL_SIZE, SYMBOL_SIZE);
-                    }
-                }
-            }
-
-            if (currentState == GameState.PLAYING) {
-                statusBar.setForeground(Color.BLACK);
-                if (currentPlayer == Seed.CROSS) {
-                    statusBar.setText("X's Turn");
-                } else {
-                    statusBar.setText("O's Turn");
-                }
-            } else if (currentState == GameState.DRAW) {
-                statusBar.setForeground(Color.RED);
-                statusBar.setText("It's a Draw! Click to play again.");
-            } else if (currentState == GameState.CROSS_WON) {
-                statusBar.setForeground(Color.RED);
-                statusBar.setText("'X' Won! Click to play again.");
-            } else if (currentState == GameState.NOUGHT_WON) {
-                statusBar.setForeground(Color.RED);
-                statusBar.setText("'O' Won! Click to play again.");
-            }
-        }
+    public GameState getCurrentState() {
+        return currentState;
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new TicTacToe();
-            }
-        });
+    public void setCurrentState(GameState currentState) {
+        this.currentState = currentState;
     }
 
+    public GameOption getCurrentOption() {
+        return currentOption;
+    }
 
+    public void setCurrentOption(GameOption currentOption) {
+        this.currentOption = currentOption;
+    }
+
+    public Seed getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Seed currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public Seed[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Seed[][] board) {
+        this.board = board;
+    }
 }
---
+++ src/TicTacToeUI.java
@@ -0,0 +1,179 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class TicTacToeUI extends JFrame {
+
+    public static final int CELL_SIZE = 100;
+    public static final int CANVAS_WIDTH = CELL_SIZE * TicTacToe.COLS;
+    public static final int CANVAS_HEIGHT = CELL_SIZE * TicTacToe.ROWS;
+    public static final int GRID_WIDTH = 8;
+    public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2;
+
+    public static final int CELL_PADDING = CELL_SIZE / 6;
+    public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING * 2;
+    public static final int SYMBOL_STROKE_WIDTH = 8;
+
+
+    private TicTacToe.GameState currentState;
+
+
+    private TicTacToe.GameOption currentOption;
+
+    private TicTacToe.Seed currentPlayer;
+
+    private TicTacToe.Seed[][] board   ;
+    private DrawCanvas canvas;
+    private JLabel statusBar;
+    TicTacToe ticTacToe;
+    Point computerChoice=null;
+
+
+    public TicTacToeUI() {
+
+        board = new TicTacToe.Seed[TicTacToe.ROWS][TicTacToe.COLS];
+        ticTacToe = new TicTacToe(currentState,currentOption,currentPlayer,board);
+        int n = JOptionPane.showConfirmDialog(
+                this,
+                "Select YES if you want two Players or NO if you want to play with the computer!",
+                "Choose game option",
+                JOptionPane.YES_NO_OPTION);
+        if (n==0){
+            ticTacToe.setCurrentOption(TicTacToe.GameOption.TWO_PlAYERS);
+        }
+        else if (n==1){
+            ticTacToe.setCurrentOption(TicTacToe.GameOption.ONE_PLAYER);
+        }
+        else {
+            System.exit(0);
+        }
+        canvas = new DrawCanvas();
+        canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+
+
+        canvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+                int rowSelected = mouseY / CELL_SIZE;
+                int colSelected = mouseX / CELL_SIZE;
+
+                if (ticTacToe.getCurrentState() == TicTacToe.GameState.PLAYING) {
+                    if (rowSelected >= 0 && rowSelected < TicTacToe.ROWS && colSelected >= 0
+                            && colSelected < TicTacToe.COLS && ticTacToe.getBoard()[rowSelected][colSelected] == TicTacToe.Seed.EMPTY) {
+
+                        ticTacToe.getBoard()[rowSelected][colSelected] = ticTacToe.getCurrentPlayer();
+                        ticTacToe.updateGame(ticTacToe.getCurrentPlayer(), rowSelected, colSelected);
+
+                        ticTacToe.setCurrentPlayer((ticTacToe.getCurrentPlayer() == TicTacToe.Seed.CROSS) ? TicTacToe.Seed.NOUGHT : TicTacToe.Seed.CROSS);
+                    }
+                }else {
+                    ticTacToe.initGame();
+                }
+
+                System.out.println("check clicks");
+                repaint();
+
+            }
+        });
+
+
+        statusBar = new JLabel("  ");
+        statusBar.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 15));
+        statusBar.setBorder(BorderFactory.createEmptyBorder(2, 5, 4, 5));
+
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(canvas, BorderLayout.CENTER);
+        cp.add(statusBar, BorderLayout.PAGE_END);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setTitle("Tic Tac Toe");
+        setVisible(true);
+
+        ticTacToe.initGame();
+    }
+
+
+
+    class DrawCanvas extends JPanel {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            setBackground(Color.WHITE);
+
+            g.setColor(Color.LIGHT_GRAY);
+            for (int row = 1; row < TicTacToe.ROWS; ++row) {
+                g.fillRoundRect(0, CELL_SIZE * row - GRID_WIDHT_HALF,
+                        CANVAS_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
+            }
+            for (int col = 1; col < TicTacToe.COLS; ++col) {
+                g.fillRoundRect(CELL_SIZE * col - GRID_WIDHT_HALF, 0,
+                        GRID_WIDTH, CANVAS_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
+            }
+
+            Graphics2D g2d = (Graphics2D)g;
+            g2d.setStroke(new BasicStroke(SYMBOL_STROKE_WIDTH, BasicStroke.CAP_ROUND,
+                    BasicStroke.JOIN_ROUND));  // Graphics2D only
+            for (int row = 0; row < TicTacToe.ROWS; ++row) {
+                for (int col = 0; col < TicTacToe.COLS; ++col) {
+                    int x1 = col * CELL_SIZE + CELL_PADDING;
+                    int y1 = row * CELL_SIZE + CELL_PADDING;
+                    if (board[row][col] == TicTacToe.Seed.CROSS) {
+                        g2d.setColor(Color.RED);
+                        int x2 = (col + 1) * CELL_SIZE - CELL_PADDING;
+                        int y2 = (row + 1) * CELL_SIZE - CELL_PADDING;
+                        g2d.drawLine(x1, y1, x2, y2);
+                        g2d.drawLine(x2, y1, x1, y2);
+                    } else if (board[row][col] == TicTacToe.Seed.NOUGHT) {
+                        g2d.setColor(Color.BLUE);
+                        g2d.drawOval(x1, y1, SYMBOL_SIZE, SYMBOL_SIZE);
+                    }
+                }
+            }
+
+
+            if (ticTacToe.getCurrentState() == TicTacToe.GameState.PLAYING) {
+                statusBar.setForeground(Color.BLACK);
+                if (ticTacToe.getCurrentPlayer() == TicTacToe.Seed.CROSS) {
+                    if (ticTacToe.getCurrentOption()==TicTacToe.GameOption.ONE_PLAYER){
+                        statusBar.setText("Computer's Turn");
+                        computerChoice = ticTacToe.computerMove(ticTacToe.getBoard());
+                        ticTacToe.getBoard()[computerChoice.x][computerChoice.y] = ticTacToe.getCurrentPlayer();
+                        ticTacToe.updateGame(ticTacToe.getCurrentPlayer(), computerChoice.x, computerChoice.y);
+                        ticTacToe.setCurrentPlayer((ticTacToe.getCurrentPlayer() == TicTacToe.Seed.CROSS) ? TicTacToe.Seed.NOUGHT : TicTacToe.Seed.CROSS);
+                    }else {
+                        statusBar.setText("X's Turn");
+                    }
+                } else {
+                    statusBar.setText("O's Turn");
+                }
+            } else if (ticTacToe.getCurrentState() == TicTacToe.GameState.DRAW) {
+                statusBar.setForeground(Color.RED);
+                statusBar.setText("It's a Draw! Click to play again.");
+            } else if (ticTacToe.getCurrentState() == TicTacToe.GameState.CROSS_WON) {
+                statusBar.setForeground(Color.RED);
+                if (ticTacToe.getCurrentOption()==TicTacToe.GameOption.ONE_PLAYER){
+                    statusBar.setText("'Computer' Won! Click to play again.");
+                }else {
+                    statusBar.setText("'X' Won! Click to play again.");
+                }
+
+            } else if (ticTacToe.getCurrentState() == TicTacToe.GameState.NOUGHT_WON) {
+                statusBar.setForeground(Color.RED);
+                statusBar.setText("'O' Won! Click to play again.");
+            }
+
+            repaint();
+        }
+    }
+
+
+}
